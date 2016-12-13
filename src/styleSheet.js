7d55ffda88b92ba32234bb87bb2661b14cf3c856
nathanmarks <info@nathanmarks.io>
2016-12-13 11:20:10 -0500

    Remove non-flow compatible docs stuff

--- src/styleSheet.js
+++ src/styleSheet.js
@@ -1,18 +1,5 @@
-/**
- * styleSheet module. Used to create styleSheet objects.
- *
- * @module styleSheet
- */
+// @flow
 
-/**
- * Core function used to create styleSheet objects
- *
- * @param  {string}   name          - Stylesheet name, should be unique
- * @param  {Function} callback      - Should return the raw rules object, passed
- *                                    `theme` as arg1
- * @param  {Object}   options       - Additional options
- * @return {Object}                 - styleSheet object
- */
 export function createStyleSheet(name, callback, options = {}) {
   const styleSheet = {
     name,
